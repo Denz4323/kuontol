5c8b7dc900695410798751c78caca7dd57e56322
Denz4323 <dsaputr847@gmail.com>
2022-12-14 11:24:59 +0700

    kuontol

---
+++ plugins/_fbc.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text }) => await conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Broadcast Here')).buffer(), `
+*ʙʀᴏᴀᴅᴄᴀsᴛ ʜᴇʀᴇ*
+
+${text}
+`.trim(), 'Denn BOT', 'ᴍᴇɴᴜ🎀', '.menu', 'sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ?', '.sc', 'ᴅᴏɴᴀsɪ🧧', '.donasi', m)
+handler.help = ['bchere <text>']
+handler.tags = ['owner']
+handler.command = ['bchere']
+handler.rowner = true
+
+export default handler
